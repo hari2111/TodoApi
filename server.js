a92d6efd98e99b08950c63a7fa292c9173df9015
Srihari <srihari2111@gmail.com>
2016-06-11 21:23:43 +0530

    ADD POST FOR TODO

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express=require('express');
+var bodyParser=require('body-parser');
 var app=express();
 var PORT=process.env.PORT || 3000;
-var todos = [{
-	id:1,
-	description:'GO sleep',
-	completed:false
-},{
-	id:2,
-	description:'get mobile',
-	completed:false
-}];
+var todoNextid=1;
+
+app.use(bodyParser.json());
+
+var todos = [];
 
 app.get('/',function (req,res) {
 	res.send('TODO API');
@@ -18,7 +15,7 @@ app.get('/todos',function(req,res){
 	res.json(todos);
 });
 app.get('/todos/:id', function(req,res){
-	var todoId=parseInt(req.params.id,10);
+	var todoId=parseInt(req.params.id,10);//converts string to number
 	var matchedtodo;
 
 	todos.forEach(function(todo){
@@ -32,7 +29,13 @@ app.get('/todos/:id', function(req,res){
 		res.status(404).send();
 	}
 });
-
+//POST
+app.post('/todos',function(req,res){
+	var body=req.body;
+	body.id=todoNextid++;
+	todos.push(body);
+	res.json(body);
+});
 
 app.listen(PORT,function(){
 	console.log('Express listening on '+PORT+ '!');
